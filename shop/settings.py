6bdf2a9bd3b58dcad8c6cbc279a0a9aa70cc58a8
Richard9703 <richardpaulov@gmail.com>
2022-06-28 09:09:16 +0000

    deployment

--- shop/settings.py
+++ shop/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
-import dj_database_url
+from pathlib import Path
 
-if os.path.isfile('env.py'):
+import dj_database_url
+if os.path.isfile("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
