67d016a00410db4447abe87f641d6915b2180a23
Richard9703 <richardpaulov@gmail.com>
2022-06-29 11:27:29 +0000

    class products and collections

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
+from .models import Collection, Product
 # from django_summernote.admin import SummernoteModelAdmin
-# from .models import Post
 
-
+# Post, Comment, 
 # @admin.register(Post)
 # class PostAdmin(SummernoteModelAdmin):
+#     list_display = ('title', 'slug', 'status', 'created_on')
+#     search_fields = ('title', 'content')
+#     prepopulated_fields = {'slug': ('title')}
 
-#     prepopulated_fields = {'slug': ('title',)}
-#     list_filter = ('status', 'created_on')
-#     summernote_fields = ('content')
+
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
+#     list_filter = ('approved', 'created_on')
+#     search_fields = ('name', 'email', 'body')
+#     actions = ['approve_comments']
+
+#     def approve_comments(self, request, queryset):
+#         queryset.update(approved=True)
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Collection, CollectionAdmin)  
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'collection', 'price', 'inventory', 'created', 'updated', 'available']
+    list_filter = ['collection', 'created', 'updated', 'available']
+    list_editable = ['inventory', 'price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin) 
--- shopapp/models.py
+++ shopapp/models.py
@@ -1,37 +1,89 @@
 from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+from django.urls import reverse
+# from django.contrib.auth.models import User
+# from cloudinary.models import CloudinaryField
 
 # Create your models here.
-STATUS = ((0, "Draft"), (1, "Published")) 
+# STATUS = ((0, "Draft"), (1, "Published")) 
 
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
+# class Post(models.Model):
+#     title = models.CharField(max_length=200, unique=True)
+#     slugh = models.SlugField(max_length=200, unique=True)
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="blog_posts"
+#     )
+#     excerpt = models.TextField(blank=True)
+#     updated_on = models.DateTimeField(auto_now=True)
+#     content = models.TextField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     status = models.IntegerField(choices=STATUS, default=0)
+#     likes = models.ManyToManyField(
+#         User, related_name='blogpost_like', blank=True)
+
+#     class Meta:
+#         ordering = ["-created_on"]
+
+#     def __str__(self):
+#         return self.title
+
+#     def number_of_likes(self):
+#         return self.likes.count()
+
+
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+#     name = models.CharField(max_length=80)
+#     email = models.EmailField()
+#     body = models.TextField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     approved = models.BooleanField(default=False)
+
+#     class Meta:
+#         ordering = ["created_on"]
+
+#     def __str__(self):
+#         return f"Comment {self.body} by {self.name}"
+
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta: 
+        ordering = ('name',)
+        verbose_name = 'collection'
+        verbose_name_plural = 'collections'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:item_list_by_collection', args=[self.slug])      
+
+
+
+
+class Product(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.DO_NOTHING, related_name='products', ) 
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now=True)
+    item_image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
+
 
     class Meta:
-        unique_together = ('slug', 'parent',)
-        verbose_name_plural = "categories"
+        ordering = ('-created',)
+        index_together = (('id', 'slug'),)
 
     def __str__(self):
-        full_path = [self.name]
-        k = self.parent
-        while k is not None:
-            full_path.append(k.name)
-            k - k.parent
-        return '->'.join(full_path[::1])    
+        return self.name
 
-class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)   
-    title = models.CharField(max_length=120) 
-    category = models.ForeignKey('Category', null=True, blank=True) 
-    content = HTMLField('Content')
-    draft = models.BooleanField(default=False)
-    publish = models.DateField(auto_now=False, auto_now_add=False,)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.title
-
-    
+    def get_absolute_url(self):
+        return reverse('shop:item_description', args=[self.id, self.slug])    
