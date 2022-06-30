6edd86665466663c4e2c408b84fe435bd3264ae6
Richard9703 <richardpaulov@gmail.com>
2022-06-30 04:22:47 +0000

    added static folder + bootstrap html

--- shop/urls.py
+++ shop/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shopapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path('about/', views.about)
 ]
--- shopapp/admin.py
+++ shopapp/admin.py
@@ -2,24 +2,6 @@ from django.contrib import admin
 from .models import Collection, Product
 
 
-# Post, Comment, 
-# @admin.register(Post)
-# class PostAdmin(SummernoteModelAdmin):
-#     list_display = ('title', 'slug', 'status', 'created_on')
-#     search_fields = ('title', 'content')
-#     prepopulated_fields = {'slug': ('title')}
-
-
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
-#     list_filter = ('approved', 'created_on')
-#     search_fields = ('name', 'email', 'body')
-#     actions = ['approve_comments']
-
-#     def approve_comments(self, request, queryset):
-#         queryset.update(approved=True)
-
 
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
