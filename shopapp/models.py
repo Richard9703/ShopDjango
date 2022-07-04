87f37b6a2227cbb92bcf008f714f0b2a2f03f180
Richard9703 <richardpaulov@gmail.com>
2022-07-04 12:53:29 +0000

    fixed url errors post & comments

--- shopapp/models.py
+++ shopapp/models.py
@@ -63,7 +63,7 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return self.title        
+        return self.name        
 
 
 class Comment(models.Model):
