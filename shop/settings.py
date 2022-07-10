06707f43d09aeff8c8f48f9efb10724ddbee9f68
Richard9703 <richardpaulov@gmail.com>
2022-07-10 08:42:31 +0000

    deployment 5

--- shop/settings.py
+++ shop/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    'cripsy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
